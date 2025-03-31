fe9ecc1bf32b1e9cae37eaf510f1b95ecdea9ada
Sunil <sunilchimankar09@gmail.com>
2025-03-31 16:32:24 +0530

    new student class added

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+   public class Student
+    {
+        public int ID { get; set; }
+    }
+}
