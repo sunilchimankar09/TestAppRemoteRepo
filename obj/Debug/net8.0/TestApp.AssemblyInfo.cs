93cafb8a1639b389ab67c689273de8386bd05b9e
Sunil <sunilchimankar09@gmail.com>
2025-03-31 16:00:50 +0530

    Added a new line of code

--- obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28723746bff83383b9e5acaf7746cca0df3bba19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
